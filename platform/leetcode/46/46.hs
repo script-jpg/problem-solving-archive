49e42f85a2d052ae7ca883b4aa7fea4d1f6b2b9a
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2025-08-11 22:47:35 -0400

    LC 46 | haskell | small refactor

--- platform/leetcode/46/46.hs
+++ platform/leetcode/46/46.hs
@@ -1,9 +1,11 @@
 import Data.List (inits, tails)
 
+splits :: [a] -> [([a], [a])]
+splits xs = zip (inits xs) (tails xs)
+
 permute :: [a] -> [[a]]
 permute [] = [[]]
 permute (x:xs) = [pre ++ [x] ++ post | rest <- permute xs, (pre, post) <- splits rest]
-  where splits a = zip (inits a) (tails a)
 
 main :: IO()
 main = print (permute [1,2,3])
