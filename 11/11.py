3fb3734064d9181a060a08a7fddb1be9f33496c9
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2025-08-15 15:48:10 -0400

    11

---
+++ 11/11.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def maxArea(height: List[int]) -> int:
+    l, r = 0, len(height) - 1
+    best = 0
+    while l < r:
+        area = (r - l) * min(height[r], height[l])
+        best = max(best, area)
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return best
